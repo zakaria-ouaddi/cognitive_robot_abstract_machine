2c6d869a1670abd9e40e9830f376d4c5ac39e78c
Simon Stelter <stelter@uni-bremen.de>
2025-07-15 13:06:34 +0200

    use qpSWIFT_sparse_bindings from pip

--- giskardpy/qp/qp_solver_qpSWIFT.py
+++ giskardpy/qp/qp_solver_qpSWIFT.py
@@ -11,7 +11,10 @@ import numpy as np
 
 from giskardpy.data_types.exceptions import QPSolverException, InfeasibleException
 from giskardpy.qp.qp_solver import QPSWIFTFormatter
-import qpSWIFT
+try:
+    import qpSWIFT_sparse_bindings as qpSWIFT
+except ImportError:
+    import qpSWIFT
 
 from giskardpy.qp.qp_solver_ids import SupportedQPSolver
 
