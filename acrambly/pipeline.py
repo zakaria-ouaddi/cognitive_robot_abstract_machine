96e1d29efa7f9d314238694403e328a5f0c68b92
till <till.kallies@gmx.de>
2026-02-13 00:56:16 +0100

    fixes to pipeline + test code

--- acrambly/pipeline.py
+++ acrambly/pipeline.py
@@ -1,102 +1,56 @@
+import geometry_msgs.msg
 import rclpy
-from lark.visitors import TransformerChain
-from perception_interfaces.msg import CubePoses
+from example_interfaces.srv import Trigger
 from perception_interfaces.srv import GetCubePoses, GetObjPoses
 
-import geometry_msgs.msg
-from example_interfaces.srv import Trigger
-
 from pycram.datastructures.pose import PoseStamped
-from rclpy import Node
-
-from semantic_digital_twin.spatial_types.spatial_types import HomogeneousTransformationMatrix as TransformationMatrix
+from pycram.ros import create_subscriber
 from semantic_digital_twin.datastructures.prefixed_name import PrefixedName
+from semantic_digital_twin.spatial_types.spatial_types import HomogeneousTransformationMatrix as TransformationMatrix
 from semantic_digital_twin.world import World
 
-from pycram.ros import create_subscriber
-
 prefix = "PhysicalObject"
-transform: TransformationMatrix
+transform = None
 
-def update_obj_positions(poses, world: World):
-    for pose in poses:
-        obj_name = pose[0]
-        obj_pose = geometry_msgs.msg.PoseStamped()
-        obj_trans = TransformationMatrix.from_xyz_quaternion(obj_pose.pose.position.x, obj_pose.pose.position.y,
-                                                             obj_pose.pose.position.z, obj_pose.pose.orientation.x,
-                                                             obj_pose.pose.orientation.y, obj_pose.pose.orientation.z,
-                                                             obj_pose.pose.orientation.w, 
-                                                             world.get_kinematic_structure_entity_by_name(
-                                                                 PrefixedName(obj_pose.header.frame_id, "tracy")))
-        if not transform:
-            body = world.get_kinematic_structure_entity_by_name(PrefixedName(obj_pose.header.frame_id, "tracy"))
-            transform = body.parent_connection.origin
-            body = body.parent_connection.parent
-            while body.name != PrefixedName(name='map', prefix='tracy'):
-                transform = transform @ body.parent_connection.origin
-                body = body.parent_connection.parent
 
-        with world.modify_world():
-            world.get_connection_by_name(PrefixedName("map_T_" + obj_name, prefix)).origin = obj_trans @ transform
-class PerceptionClient:
+class PerceptionClientSingle():
     def __init__(self, world, node):
         self.world = world
         self.node = node
-        self.client = self.node.create_client(Trigger, "save_camera_frames")
-        create_subscriber("/Current_OBJ_position_0", geometry_msgs.msg.PoseStamped, self.callback0, 10)
-        create_subscriber("/Current_OBJ_position_1", geometry_msgs.msg.PoseStamped, self.callback1, 10)
-        create_subscriber("/Current_OBJ_position_2", geometry_msgs.msg.PoseStamped, self.callback2, 10)
-        self.cube1_pose = PoseStamped()
-        self.cube2_pose = PoseStamped()
-        self.cube3_pose = PoseStamped()
-        self.req = Trigger.Request()
-
-    def callback0(self, msg):
-        self.cube1_pose = msg
-        print("received cube1 pose")
-
-    def callback1(self, msg):
-        self.cube2_pose = msg
-        print("received cube2 pose")
-
-    def callback2(self, msg):
-        self.cube3_pose =msg
-        print("received cube3 pose")
-
-    def request(self):
-        future = self.client.call_async(self.req)
-        rclpy.spin_until_future_complete(self.node, future)
-        response = future.result()
-        print(response)
-        poses = [("Cube_1", self.cube1_pose), ("Cube_2", self.cube2_pose), ("Cube_3", self.cube3_pose)]
-        print(poses)
-        update_obj_positions(poses, self.world)
-
-class PerceptionClientNew:
-    def __init__(self, world, node):
-        self.world = world
-        self.node = node
-        self.client = self.node.create_client(GetCubePoses, "save_camera_frames")
-        self.req = GetCubePoses.Request()
-
-    def request(self):
-        future = self.client.call_async(self.req)
-        rclpy.spin_until_future_complete(self.node, future)
-        response = future.result()
-        poses = [("Cube_1", response.cube_1_pose), ("Cube_2", response.cube_2_pose), ("Cube_3", response.cube_3_pose)]
-        update_obj_positions(poses, self.world)
-
-class PerceptionClientSingle:
-    def __init__(self, world, node):
-        self.world = world
-        self.node = node
-        self.client = self.node.create_client(GetObjPoses, "save_camera_frames")
+        self.client = self.node.create_client(GetObjPoses, "cube_poses")
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.node.get_logger().info('service not available, waiting again...')
         self.req = GetObjPoses.Request()
+        self.transform = None
+
+    def update_obj_positions(self, poses, world: World):
+        for pose in poses:
+            obj_name = pose[0]
+            obj_pose = geometry_msgs.msg.PoseStamped()
+            obj_trans = TransformationMatrix.from_xyz_quaternion(obj_pose.pose.position.x, obj_pose.pose.position.y,
+                                                                 obj_pose.pose.position.z, obj_pose.pose.orientation.x,
+                                                                 obj_pose.pose.orientation.y, obj_pose.pose.orientation.z,
+                                                                 obj_pose.pose.orientation.w,
+                                                                 world.get_kinematic_structure_entity_by_name(
+                                                                     PrefixedName("camera_link", "tracy")))
+            if not self.transform:
+                body = world.get_kinematic_structure_entity_by_name(PrefixedName("camera_link", "tracy"))
+                self.transform = body.parent_connection.origin
+                body = body.parent_connection.parent
+                while body.name != PrefixedName(name='map', prefix='tracy'):
+                    print(self.transform)
+                    print(body.parent_connection.origin)
+                    self.transform = self.transform @ body.parent_connection.origin
+                    print(self.transform)
+                    body = body.parent_connection.parent
+
+            with world.modify_world():
+                world.get_connection_by_name(PrefixedName("map_T_" + obj_name, prefix)).origin = obj_trans @ self.transform
 
     def request(self, obj):
         self.req.object = obj
         future = self.client.call_async(self.req)
         rclpy.spin_until_future_complete(self.node, future)
         response = future.result()
-        poses = [(obj, response.cube_1_pose)]
-        update_obj_positions(poses, self.world)
+        poses = [(obj, response.pose)]
+        self.update_obj_positions(poses, self.world)
---
+++ acrambly/testpipeline.py
@@ -0,0 +1,81 @@
+import rclpy
+from semantic_digital_twin.spatial_computations.raytracer import RayTracer
+
+from semantic_digital_twin.world_description.connections import Connection6DoF
+
+from acrambly.pipeline import PerceptionClientSingle
+from semantic_digital_twin.adapters.urdf import URDFParser
+
+from semantic_digital_twin.world_description.shape_collection import ShapeCollection
+
+from semantic_digital_twin.world_description.geometry import Box, Scale, Color
+
+from semantic_digital_twin.datastructures.prefixed_name import PrefixedName
+
+from semantic_digital_twin.world_description.world_entity import Body
+
+from semantic_digital_twin.world import World
+
+from semantic_digital_twin.spatial_types.spatial_types import HomogeneousTransformationMatrix as tm
+
+if not rclpy.ok():
+    rclpy.init()
+node = rclpy.create_node("semantic_world")
+
+sw = World()
+# root = Body(name= PrefixedName('root', "world"))
+body_box1 = Body(
+    name=PrefixedName("blue_box", "PhysicalObject"),
+)
+body_box2 = Body(
+    name=PrefixedName("red_box", "PhysicalObject"),
+)
+body_box3 = Body(
+    name=PrefixedName("yellow_box", "PhysicalObject"),
+)
+box1_start = [0.75, 0.00, 0.9, 0, 0, 0]
+box2_start = [0.75, 0.50, 0.9, 0, 0, 0]
+box3_start = [0.75, 0.25, 0.9, 0, 0, 0]
+
+box1_target = [0.75, 0.50, 0.07, 0, 0, 0]
+box2_target = [0.75, 0.50, 0.12, 0, 0, 0]
+box3_target = [0.75, 0.50, 0.19, 0, 0, 0]
+
+box1 = Box(tm(reference_frame=body_box1),scale=Scale(0.05,0.05,0.05), color=Color(1,0,0,1))
+box2 = Box(tm(reference_frame=body_box2),scale=Scale(0.05,0.05,0.05), color=Color(0,1,0,1))
+box3 = Box(tm(reference_frame=body_box3),scale=Scale(0.05,0.05,0.05), color=Color(0,0,1,1))
+
+
+body_box1.collision = body_box1.visual = ShapeCollection([box1], body_box1)
+body_box2.collision = body_box2.visual = ShapeCollection([box2], body_box2)
+body_box3.collision = body_box3.visual = ShapeCollection([box3], body_box3)
+
+tracy_world = URDFParser.from_file("../semantic_digital_twin/resources/urdf/tracy.urdf").parse()
+#tracy_world = URDFParser.from_file(os.path.join(os.path.dirname(__file__), "..", "resources", "robots", "tracy.urdf")).parse()
+from semantic_digital_twin.robots.tracy import Tracy
+robot_view = Tracy.from_world(tracy_world)
+root = tracy_world.root
+rt = RayTracer(tracy_world)
+with tracy_world.modify_world():
+    c_root_box1 = Connection6DoF.create_with_dofs(world=tracy_world, parent=root, child=body_box1)
+    tracy_world.add_connection(c_root_box1)
+
+    c_root_box2 = Connection6DoF.create_with_dofs(world=tracy_world, parent=root, child=body_box2)
+    tracy_world.add_connection(c_root_box2)
+
+    c_root_box3 = Connection6DoF.create_with_dofs(world=tracy_world, parent=root, child=body_box3)
+    tracy_world.add_connection(c_root_box3)
+    c_root_box1.origin = tm.from_xyz_rpy(*box1_start, reference_frame=root, child_frame=body_box1)
+    c_root_box2.origin = tm.from_xyz_rpy(*box2_start, reference_frame=root)
+    c_root_box3.origin = tm.from_xyz_rpy(*box3_start, reference_frame=root)
+
+perception = PerceptionClientSingle(tracy_world, node)
+rt.update_scene()
+rt.scene.show()
+perception.request("blue_box")
+print("Done")
+
+rt.update_scene()
+rt.scene.show()
+
+print(perception.transform)
